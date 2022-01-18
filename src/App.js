2c265b1e3b316b4c8ec99a43a057833bad14c262
ronbrown3 <73085072+ronbrown3@users.noreply.github.com>
2022-01-18 09:18:28 -0600

    Add contact banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import ghlogo from './assets/github.png';
+import twlogo from './assets/twitter.png';
 
 const App = () => {
   useEffect(() => {
@@ -16,6 +18,30 @@ const App = () => {
         <h3>
           I am a <span className="green">Full Stack Developer.</span>
         </h3>
+      </div>
+
+      <div className="banner">
+        <div className="inline-block">
+          <a href="https://github.com/ronbrown3">
+            <img
+              src={ghlogo}
+              alt="Check out my work on Github!"
+              width="30"
+              height="30"
+            />
+          </a>
+        </div>
+
+        <div className="inline-block">
+          <a href="https://twitter.com/rondevops">
+            <img
+              src={twlogo}
+              alt="Contact me on Twitter!"
+              width="30"
+              height="30"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
